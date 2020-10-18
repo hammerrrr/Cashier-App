4230acbf42393c1df70c579b1825bd6ebcfd3246
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-19 06:42:07 +0700

    add getter setter class item

--- Cashier App/Item.cs
+++ Cashier App/Item.cs
@@ -15,7 +15,18 @@ namespace Cashier_App
 
         public Item(int id, string title, int jumlah, string type, double price)
         {
-
+            this.id = id;
+            this.title = title;
+            this.jumlah = jumlah;
+            this.price = price;
+            this.subtotal = 0;
+            this.type = type;
         }
+
+        public string getTtitle() => title;
+        public int getJumlah() => jumlah;
+        public string getType() => type;
+        public double getPrice() => price;
+        public double getSubtotal() => price * jumlah;
     }
 }
