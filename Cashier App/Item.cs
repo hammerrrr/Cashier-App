bf55bbfb07785d17e2ea851682e7d5637a2dbd1e
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-10-19 06:38:15 +0700

    Add project files.

---
+++ Cashier App/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cashier_App
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int jumlah { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string title, int jumlah, string type, double price)
+        {
+
+        }
+    }
+}
